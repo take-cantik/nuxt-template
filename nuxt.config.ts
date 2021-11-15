c42d6e281a4b7c562f0e77ccee309dd7acdf4555
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-11-16 00:03:38 +0900

    Add: ress and sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["ress/dist/ress.min.css", "swiper/css/swiper.css"],
+
+  styleResources: {
+    scss: ["~assets/vars/*.scss"],
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
