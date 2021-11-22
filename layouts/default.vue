3f632538f75bec4051205e195a024857e5ea47bb
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-11-22 22:42:42 +0900

    Update: default html style:wq

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,23 @@
 <template>
   <Nuxt />
 </template>
+
+<style lang="scss">
+html {
+  font-size: 10px;
+  font-weight: 400;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+
+.hide {
+  display: none !important;
+}
+
+.permeation {
+  opacity: 0.5 !important;
+}
+</style>
